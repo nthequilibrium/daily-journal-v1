e60903b10c5ace5c51eb2db24c393946125470d4
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-02-26 13:48:01 +0530

    Add 'home' route to the blog

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const { RSA_NO_PADDING } = require("constants");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,3 +15,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+    res.render("home", {msg: "Home"});
+})
+
+app.listen(3000, () => {
+    console.log("Server running @ port 3000...");
+});
